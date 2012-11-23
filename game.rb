4d493dc7d0698fb92478f67b8e9f2d49d7448d5e
Adam Hindorff Claesson <Adam.HindorffClaesson@elev.it-gymnasiet.se>
2012-11-23 10:23:56 +0100

    game 1

---
+++ game.rb
@@ -0,0 +1,23 @@
+require 'chingu'
+
+class Game < Chingu::Window
+
+	#constructor
+	def initialize 
+			super
+		self.input = {esc: :exit}
+		@player = Player.create
+	end
+end
+
+class Player < Chingu::GameObject
+
+	#meta-constructor
+	def setup
+		@x, @y = 350, 400
+		@image = Gosu::Image["ship.png"]
+	end
+
+end
+
+Game.new.show
